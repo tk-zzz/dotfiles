254f3199838e6c4da49db6446b2a05f120319112
ajiwoKutta <49810315+ajiwoKutta@users.noreply.github.com>
2020-03-03 00:12:50 +0900

    Mod not to link .gitigonre and .gitmodules

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ DOTPATH=~/dotfiles
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
     ln -sf "$DOTPATH/$f" "$HOME"/"$f"
 done
