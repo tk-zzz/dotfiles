f74e9206c329986d3cdc14f62435805472ff000c
ajiwoKutta <49810315+ajiwoKutta@users.noreply.github.com>
2020-03-03 01:10:22 +0900

    Add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+#dotfiles
+
+##使い方
+homeディレクトリでgit clone
+./install.sh
+でホームディレクトリにシンボリックリンクを張ります。
+
+##molokai
+このリポジトリにはmolokaiがサブモジュールとして追加されています。
+./pull_molokaiでダウンロードから、vimへの設定まで行います。
+install.shとどちらが先でも問題ありません。
--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitignore" ] && continue
     [ "$f" = ".gitmodules" ] && continue
+    [ "$f" = "README.md" ] && continue
     ln -sf "$DOTPATH/$f" "$HOME"/"$f"
 done
