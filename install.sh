389fa895bc5dec37d5469983a2ac275fcff11f5c
ajiwoKutta <49810315+ajiwoKutta@users.noreply.github.com>
2020-03-05 23:47:58 +0900

    Mod install logic and change resource directory

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-DOTPATH=~/dotfiles
+RESOURCEPATH=$(pwd)/resources
 
+cd $RESOURCEPATH
 for f in .??*
 do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitignore" ] && continue
-    [ "$f" = ".gitmodules" ] && continue
-    [ "$f" = "README.md" ] && continue
-    ln -sf "$DOTPATH/$f" "$HOME"/"$f"
+    if [ -f "$HOME/$f" -a ! -L "$HOME/$f" ]; then
+        echo "back up $f in /$(date "+%Y%m%d")bk"
+        mkdir ../$(date "+%Y%m%d")bk
+        cp "$HOME/$f" "../$(date "+%Y%m%d")bk/"
+    fi
+    ln -sf "$RESOURCEPATH"/"$f" "$HOME"/"$f"
 done
+cd ..
