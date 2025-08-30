b223a11ada1e931672d46cccfb407646249f7d39
tk-zzz <49810315+tk-zzz@users.noreply.github.com>
2025-08-31 02:19:36 +0900

    stylua

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,71 +1,89 @@
 -- lazy bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim", lazypath })
 end
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
-  { "folke/tokyonight.nvim", lazy = false, priority = 1000,
-    config = function() vim.cmd.colorscheme("tokyonight-night") end },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("tokyonight-night")
+    end,
+  },
 
-  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function() require("lualine").setup({}) end },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({})
+    end,
+  },
 
   { "folke/which-key.nvim", config = true },
   { "numToStr/Comment.nvim", config = true },
   { "lewis6991/gitsigns.nvim", config = true },
 
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local t = require("telescope")
       t.setup({})
       local map = vim.keymap.set
       map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
-      map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>",  { desc = "Live grep" })
-      map("n", "<leader>fb", "<cmd>Telescope buffers<cr>",    { desc = "Buffers" })
-      map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>",  { desc = "Help" })
-    end
+      map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
+      map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Buffers" })
+      map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help" })
+    end,
   },
   -- 任意で FZF
   -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make",
   --   config = function() require("telescope").load_extension("fzf") end },
 
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = { "lua", "go", "typescript", "javascript", "tsx", "json", "html", "css", "c_sharp" },
         highlight = { enable = true },
         indent = { enable = true },
       })
-    end
+    end,
   },
 
-  { "stevearc/oil.nvim",
+  {
+    "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("oil").setup()
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end
+    end,
   },
 
-  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("nvim-tree").setup({
         view = { width = 30 },
         filters = { dotfiles = false },
       })
       vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file explorer" })
-    end
+    end,
   },
 
   { "neovim/nvim-lspconfig" },
   { "williamboman/mason.nvim", config = true },
-  { "williamboman/mason-lspconfig.nvim",
+  {
+    "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" },  -- 必要なときだけ増やす
+        ensure_installed = { "lua_ls" }, -- 必要なときだけ増やす
         automatic_installation = false,
       })
       require("lspconfig").lua_ls.setup({
@@ -76,10 +94,11 @@ require("lazy").setup({
           },
         },
       })
-    end
+    end,
   },
 
-  { "hrsh7th/nvim-cmp",
+  {
+    "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
@@ -101,7 +120,7 @@ require("lazy").setup({
           { name = "path" },
         }),
       })
-  
+
       -- cmdline補完設定
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
@@ -110,14 +129,23 @@ require("lazy").setup({
           { name = "cmdline" },
         },
       })
-  
+
       cmp.setup.cmdline("/", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
-          { name = "buffer" }
+          { name = "buffer" },
         },
       })
     end,
   },
-})
 
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      format_on_save = { timeout_ms = 2000, lsp_fallback = true },
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+    },
+  },
+})
